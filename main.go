1df511494ebc0aabb7c8fed19a46edb413e2432c
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-05-25 20:13:26 +0200

    remove unneeded files

--- main.go
+++ main.go
@@ -12,5 +12,12 @@ func main() {
 		}`))
 	})
 
+	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
+		/* TODO
+		* List all the needed todo items in todoList
+		* Add json responding with a list of the todo list
+		 */
+	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
