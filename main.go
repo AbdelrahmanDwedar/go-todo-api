12f5a3e8a7ff5c84f7b6eaa6d9bf5df74524d1ad
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-05-31 23:27:37 +0200

    add GetAll functionality

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
+	store, err := NewSqliteStore("database.db")
+	if err != nil {
+		log.Fatalf("Failed to initialize store: %v", err)
+	}
+
+	server := NewServer(store)
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`{
 			"message": "pong"
@@ -13,10 +21,28 @@ func main() {
 	})
 
 	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
-		/* TODO
-		* List all the needed todo items in todoList
-		* Add json responding with a list of the todo list
-		 */
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		todoLists, err := server.store.GetAll()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to fetch todo lists"))
+			return
+		}
+
+		jsonBytes, err := json.Marshal(todoLists)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to marshal todo lists"))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(jsonBytes)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
